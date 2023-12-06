f9f6d4c79b4719149844dd11c236368a780d7ac6
Kauan Silva da Costa <100085923+Kauan678f@users.noreply.github.com>
2023-12-05 23:31:00 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+// local onde ocorre a criação da lista e as chamadas das funções
+
+import list from "./list.js"
+// console.log(list)
+
+let listUse = new list();
+listUse.insert(22, 7)
+listUse.add(14)
+listUse.add(41)
+listUse.add(56)
+listUse.insert(12)
+// listUse.delete(0)
+// listUse.delete(2)
+console.log(listUse.print())
+// listUse.add(234)
+// listUse.add(564)
+// listUse.add(457) 
+// listUse.add(7609)
+// console.log(list)
+// console.log(listUse.print())
+// listUse.insert(22, 2)
+// listUse.insert(22, 4)
+// listUse.insert(22, 6)
+// listUse.delete(4)
+// console.log(listUse.print())
+// listUse.delete(2)
+// listUse.delete()
+// console.log(listUse.print())
+
+// console.log(listUse.printPosition(0))
+// console.log(listUse.printIndex(457))
+// console.log(listUse.print())
+// console.log(listUse.len())
---
+++ noList.js
@@ -0,0 +1,11 @@
+// O no da lista
+
+class noList{
+    constructor(data, position){
+        this.next = null
+        this.data = data
+        // this.position = position
+    }
+}
+
+export default noList
